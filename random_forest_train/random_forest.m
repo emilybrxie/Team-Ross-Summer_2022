0aafd483dd5aa79a9753f8847630ff24b8f4890a
emilybrxie <98119821+emilybrxie@users.noreply.github.com>
2022-07-19 22:49:03 +0800

    updated rf model

--- random_forest_train/random_forest.m
+++ random_forest_train/random_forest.m
@@ -3,10 +3,22 @@ clear all
 close all
 warning off
 
-hr_data=readtable('test_hr_data.csv');
+hr_data=readtable('3_18_data.csv');
+
+%fill in NaNs in time & 0 in bpm
+rows=size(hr_data,1);
+for row = 1:rows
+    A=hr_data{row,:};
+    TF=isnan(A);
+    if TF(1)==true
+        hr_data(row,1)=hr_data(row-1,1);
+        hr_data(row,2)=hr_data(row-1,2);
+    end
+end
+
 
 %training the model
-cv=cvpartition(size(hr_data,1),'HoldOut',0.3);
+cv=cvpartition(size(hr_data,1),'HoldOut',0.2);
 index=cv.test;
 dTrain=hr_data(~index,2:end);
 dTest=hr_data(index,2:end);
@@ -15,14 +27,17 @@ model=fitensemble(dTrain,'is_affect','Bag',100,'Tree','Type','classification');
 prediction=predict(model,testing);
 
 %accuracy of prediction (this part I looked up)
-ms=(sum(prediction==table2array(dTest(:,end)))/size(dTest,1))*100
+maccuracy=(sum(prediction==table2array(dTest(:,end)))/size(dTest,1))*100
 
-%visualizing classification outcome
-a=min(hr_data.bpm):0.1:max(hr_data.bpm);
-b=min(hr_data.rr):0.1:max(hr_data.rr);
-[x1 x2]=meshgrid(a,b);
-x=[x1(:) x2(:)];
-ms=predict(model,x);
-gscatter(x1(:),x2(:),ms,'cy');
-hold on;
-gscatter(dTrain.bpm,dTrain.rr,dTrain.is_affect,'rg','.',30);
+%feature importance
+feature_importance=oobPermutedPredictorImportance(model);
+figure
+bar(feature_importance)
+title('Feature Importance Estimates')
+xlabel('Predictor variable')
+ylabel('Importance')
+h=gca;
+h.XTickLabel=model.PredictorNames;
+h.XTickLabelRotation=45;
+h.TickLabelInterpreter='none';
+
